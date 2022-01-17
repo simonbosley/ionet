e7baae6c812d30812ca5f56fa2d8a3e9fc4fee42
Simon Bosley <sbosley@solidstatelogic.com>
2022-01-17 13:43:19 +0000

    Adding python install to database server dockerfile, for the pending data import scripts

--- database/run_docker.sh
+++ database/run_docker.sh
@@ -1,5 +1,11 @@
 # Run the database docker image that we built (if it doesn't exist, build first).
 docker run --name database_container -p 5432:5432 -d database_image
 
-# Print the logs from the server as it's starting up... TODO: Set this to a time period so we don't keep following it.
-docker logs --details --follow database_container
+# The container is now hopefully running in detached mode, let's give it some time to start up.
+sleep 1
+
+# Print the logs from the server now that it has had a chance to start up.
+docker logs --details --timestamps database_container
+
+# Get the version of python installed.
+docker exec database_container /usr/bin/python3 --version
