d8cd24c1151fe200ccc2c8589df170589ac75a40
Simon Bosley <sbosley@solidstatelogic.com>
2022-01-22 21:50:15 +0000

    Setting up a flask app to serve a test message on port 5000.

--- app_server/run_docker.sh
+++ app_server/run_docker.sh
@@ -1,5 +1,5 @@
 # Run the app server docker image that we built (if it doesn't exist, build first).
-docker run --name app_server_container -d app_server_image:1.0.0
+docker run --name app_server_container -dp 5000:5000 app_server_image:1.0.0
 
 # The container is now hopefully running in detached mode, let's give it some time to start up.
 sleep 1
